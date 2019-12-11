1d0c3cdf1a28d8ed3aed3a9f3b5ea9faec726f79
Everton Miranda Vitório <c1300290@interno.bb.com.br>
2019-12-11 14:27:15 -0300

    E-mail duplicado

--- __tests__/integration/User.test.js
+++ __tests__/integration/User.test.js
@@ -1,16 +1,35 @@
 import request from 'supertest';
 import app from '../../src/app';
+import truncate from '../util/truncate';
+
+const requestMock = {
+  name: 'Everton Miranda',
+  email: 'everton.mirandav@gmail.com',
+  password_hash: '123456',
+};
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Everton Miranda',
-        email: 'everton.mirandav@gmail.com',
-        password_hash: '123456',
-      });
+      .send(requestMock);
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send(requestMock);
+
+    const response = await request(app)
+      .post('/users')
+      .send(requestMock);
+
+    expect(response.status).toBe(400);
   });
 });
